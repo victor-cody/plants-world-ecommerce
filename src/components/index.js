259fd8cb81812f16e9b8b9c810692e36fb05dd0d
Victor Ikechukwu <vickyikechukwu13@gmail.com>
2023-03-03 00:36:55 +0100

    export components out of index.js

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+// Storyblok components
+export { default as Teaser } from "./storyblok/Teaser";
+export { default as Page } from "./storyblok/Page";
+export { default as Plant } from "./storyblok/Plant";
+export { default as FeaturedPlants } from "./storyblok/FeaturedPlants";
+
+// components
+export { default as PlantCard } from './PlantCard.js'
--- src/components/storyblok/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Teaser } from "./Teaser";
-export { default as Page } from "./Page";
-export { default as Plant } from "./Plant";
-export { default as FeaturedPlants } from "./FeaturedPlants";
