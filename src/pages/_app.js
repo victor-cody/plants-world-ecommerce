616c1bee490f4e108e7b950ea78a15edde618615
Victor Ikechukwu <vickyikechukwu13@gmail.com>
2023-03-04 01:13:06 +0100

    fixes to connect to storyblok visual editor

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "@styles/Home.module.css";
+
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import { Teaser, Page, Plant, FeaturedPlants } from "../components/";
+
+const components = {
+  teaser: Teaser,
+  page: Page,
+  plant: Plant,
+  featured_plants: FeaturedPlants,
+};
+
+storyblokInit({
+  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_API_ACCESS_TOKEN,
+  use: [apiPlugin],
+  components,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
